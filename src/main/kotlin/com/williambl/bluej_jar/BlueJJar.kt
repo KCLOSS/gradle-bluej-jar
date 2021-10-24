8ff928404bfc78f1c47570dc711f90bb5fc9350e
williambl <william@williambl.com>
2021-10-24 20:13:32 +0100

    Initial Commit

---
+++ src/main/kotlin/com/williambl/bluej_jar/BlueJJar.kt
@@ -0,0 +1,10 @@
+package com.williambl.bluej_jar
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class BlueJJar: Plugin<Project> {
+    override fun apply(project: Project) {
+        TODO("Not yet implemented")
+    }
+}
