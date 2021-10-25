bd8f68e8399e88a65d5da0a26d1c8c5354af7602
williambl <william@williambl.com>
2021-10-25 10:10:32 +0100

    Convert bluej jar task method to extension function

--- src/main/kotlin/com/williambl/bluej_jar/BlueJJar.kt
+++ src/main/kotlin/com/williambl/bluej_jar/BlueJJar.kt
@@ -2,33 +2,38 @@ package com.williambl.bluej_jar
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.jvm.tasks.Jar
 
 class BlueJJar: Plugin<Project> {
     override fun apply(project: Project) {
-        project.tasks.register("blueJJar", Jar::class.java) { task ->
-            task.group = "build"
-            task.description = "Build a JAR file which can be imported as a BlueJ Project"
-            task.archiveClassifier.set("bluej")
-
-            val java = project.extensions.getByType(JavaPluginExtension::class.java)
-
-            val mainSourceSet = java.sourceSets.getAt("main")
-
-            task.dependsOn(mainSourceSet.runtimeClasspath)
-
-            task.from(mainSourceSet.output)
-            task.from(mainSourceSet.java)
-
-            task.from({ mainSourceSet.runtimeClasspath.filter { it.name.endsWith("jar") }.map { project.zipTree(it) } }) {
-                it.exclude("META-INF/**")
-            }
-            task.from({ mainSourceSet.runtimeClasspath.filter { it.name.endsWith("jar") } }) {
-                it.into("+libs")
-            }
-            task.from( { mainSourceSet.runtimeClasspath.filter { it.name.equals("README.TXT", true) }.map { project.zipTree(it) }})
-        }
+        project.tasks.register("blueJJar", Jar::class.java, Jar::blueJ)
     }
+}
+
+fun Jar.blueJ() {
+    group = "build"
+    description = "Build a JAR file which can be imported as a BlueJ Project"
+    archiveClassifier.set("bluej")
+
+    val java = project.extensions.getByType(JavaPluginExtension::class.java)
+
+    val mainSourceSet = java.sourceSets.getAt("main")
+
+    dependsOn(mainSourceSet.runtimeClasspath)
+
+    from(mainSourceSet.output)
+    from(mainSourceSet.java)
+
+    from({
+        mainSourceSet.runtimeClasspath.filter { it.name.endsWith("jar") }.map { project.zipTree(it) }
+    }) {
+        it.exclude("META-INF/**")
+    }
+    from({ mainSourceSet.runtimeClasspath.filter { it.name.endsWith("jar") } }) {
+        it.into("+libs")
+    }
+    from({
+        mainSourceSet.runtimeClasspath.filter { it.name.equals("README.TXT", true) }.map { project.zipTree(it) }
+    })
 }
